ab6b6fc5c1b29d11af32a7dcc2d0e502152748ca
Wharley01 <hackerslord96@gmail.com>
2022-01-04 13:18:35 +0100

    Added Support for fixed size string column (VARCHAR)

--- dataTypes/Text.ts
+++ dataTypes/Text.ts
@@ -1,11 +1,11 @@
 import {DataTypes} from "sequelize";
 import DataType from "./DataType";
-import Model from "../model/model";
+import {StringDataType} from "sequelize/types/lib/data-types";
 
-export default class Text extends DataType<string>{
+export default class Text extends DataType<StringDataType|DataTypes.TextDataTypeConstructor>{
     getType() {
         if (this.size){
-            return DataTypes.STRING(this.size)
+            return DataTypes.STRING(typeof this.size == 'number' ? this.size:undefined)
         }else{
             return DataTypes.TEXT
         }
