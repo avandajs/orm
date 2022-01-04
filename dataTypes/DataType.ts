1a6958b810dff33d4613ee433b05aae5a2fa4462
Wharley01 <hackerslord96@gmail.com>
2022-01-04 13:17:01 +0100

    Add base datatype

--- dataTypes/DataType.ts
+++ dataTypes/DataType.ts
@@ -1,15 +1,17 @@
-import {AbstractDataTypeConstructor, StringDataType, TextDataTypeConstructor} from "sequelize/types/lib/data-types";
 import Model from "../model/model";
 
-export default abstract class DataType<Type>{
+export default abstract class DataType<ReturnType>{
     isNullable?: boolean
     value?: unknown
     references?: Model
-    size?: number
-    data?: Type
-    constructor(default_value?: Type) {
+    size?: number[] | number
+    args?: any[]
+    data?: any
+    getter?: <T> (value: unknown) => unknown
+    setter?: <T> (value: unknown) => unknown
+    constructor(default_value?: any) {
         this.data = default_value
         return this;
     }
-    abstract getType(): AbstractDataTypeConstructor | StringDataType | TextDataTypeConstructor | undefined
+    abstract getType(): ReturnType
 }
