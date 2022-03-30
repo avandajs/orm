d63db1efc2addd8784676568c22f53e1ed357bd8
Wharley01 <hackerslord96@gmail.com>
2022-03-30 07:57:53 +0100

    Added support for geocolumn

--- dataTypes/DataType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class DataType {
-    constructor(default_value) {
-        this.data = default_value;
-        return this;
-    }
-}
-exports.default = DataType;
--- index.ts
+++ index.ts
@@ -4,11 +4,13 @@ import Column from "./model/column";
 import Fnc from "./model/fnc";
 import ColumnNames from "./types/ColumnNames";
 import DataOf from "./types/DataOf";
+import GeoPoint from "./types/GeoPoint";
 export {
     Model,
     Column,
     Fnc,
     Seeder,
     ColumnNames,
-    DataOf
+    DataOf,
+    GeoPoint
 }
